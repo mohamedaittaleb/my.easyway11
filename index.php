80e817ba4ecc71d699812602fe0f1b40d4ab5b2e
mohamedaittaleb <mohamedaittaleb1994@gmail.com>
2016-12-10 14:10:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -15,18 +15,33 @@ if(!empty($messageText))
 {
 	$menu_message = [];
 	$buttons = [];
-	$buttons[] = ['type' => 'postback', 'title' => 'Ask us a Question', 'payload' => 'CURRENT_MENU'];
+	$buttons[] = ['type' => 'postback', 'title' => 'Ask us a Question', 'payload' => 'ASK_QUESTION'];
 	$buttons[] = ['type' => 'postback', 'title' => 'Use Dictionary', 'payload' => 'TODAY_DEALS'];
 		$menu_message = [
               'type'    => 'template',
              'payload' => ['template_type' => 'button', 'text' => 'Welcome to our facebook page My Easy Way To Learn English', 'buttons' => $buttons]
             
              ];
- 
-		$response = [
+	$response = [
 		'recipient' => [ 'id' => $senderId ],
-		'message' => [ 'attachment' => $menu_message+$messageText ]
+		'message' => [ 'attachment' => $menu_message ]
 		];
+ 	if ($messageText == 'ASK_QUESTION') {
+		$menu_message = [
+            'type'    => 'template',
+            'payload' => [
+                'template_type' => 'button',
+                'text'          => 'Do you want to see your order?',
+                'buttons'       => $buttons
+            ]
+        ];
+        $response = [
+		'recipient' => [ 'id' => $senderId ],
+		'message' => [ 'attachment' => $menu_message ]
+		];
+	}
+		
+
 		$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
