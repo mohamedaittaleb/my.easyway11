70e6556ee57afe35e2ab2ae14f8bfde8b112abd7
mohamedaittaleb <mohamedaittaleb1994@gmail.com>
2016-12-10 15:46:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,24 +12,8 @@ $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $special_command = $input['entry'][0]['messaging'][0]['postback']['payload'];
-if(!empty($messageText))
-{
-	$menu_message = [];
-	$buttons = [];
-	$buttons[] = ['type' => 'postback', 'title' => 'Ask us a Question', 'payload' => 'ASK_QUESTION'];
-	$buttons[] = ['type' => 'postback', 'title' => 'Use Dictionary', 'payload' => 'USE_DIC'];
-	$menu_message = [
-	'type'    => 'template',
-	'payload' => ['template_type' => 'button', 'text' => 'Welcome to our facebook page My Easy Way To Learn English', 'buttons' => $buttons]
 
-	];
-
-	$response = [
-		'recipient' => [ 'id' => $senderId ],
-		'message' => [ 'attachment' => $menu_message ]
-	];
-
-	if (strpos($messageText, 'translate') !== false) {
+if (strpos($messageText, 'translate') !== false) {
 	$apiKey = 'AIzaSyDwFOPkNAENeet5eTqaT6g18Eua5iQbn0I';
     	$text = $messageText;
     	$url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=en&target=ar';
@@ -46,8 +30,6 @@ if(!empty($messageText))
 		'message' => [ 'text' => $responseDecoded['data']['translations'][0]['translatedText'] ]
 	];
 	
-	}
-		
 }elseif ($special_command == 'ASK_QUESTION') {
 		$ask_message = 'Please write your question :) ';
 		
@@ -67,6 +49,21 @@ elseif ($special_command == 'USE_DIC') {
 		];
 	
 	
+}else{
+	$menu_message = [];
+	$buttons = [];
+	$buttons[] = ['type' => 'postback', 'title' => 'Ask us a Question', 'payload' => 'ASK_QUESTION'];
+	$buttons[] = ['type' => 'postback', 'title' => 'Use Dictionary', 'payload' => 'USE_DIC'];
+	$menu_message = [
+	'type'    => 'template',
+	'payload' => ['template_type' => 'button', 'text' => 'Welcome to our facebook page My Easy Way To Learn English', 'buttons' => $buttons]
+
+	];
+
+	$response = [
+		'recipient' => [ 'id' => $senderId ],
+		'message' => [ 'attachment' => $menu_message ]
+	];
 }
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 	curl_setopt($ch, CURLOPT_POST, 1);
